b9e95748316fa8bb7a8c96a002a84294a40fa445
punmechanic <2939173+punmechanic@users.noreply.github.com>
2024-04-18 20:58:47 -0700

    Initial Homelab commit

---
+++ dns/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    gandi = {
+      version = "2.3.0"
+      source  = "go-gandi/gandi"
+    }
+  }
+}
+
+provider "gandi" {}
+
+resource "gandi_domain" "aredherring_tech" {
+  name      = "aredherring.tech"
+  autorenew = true
+  owner {
+    city            = "Dublin"
+    country         = "IE"
+    data_obfuscated = true
+    email           = "aredherring2+gandi@gmail.com"
+    extra_parameters = {
+      "birth_city"       = ""
+      "birth_country"    = ""
+      "birth_date"       = ""
+      "birth_department" = ""
+    }
+    family_name     = "Pantry"
+    given_name      = "Dan"
+    mail_obfuscated = true
+    phone           = "+353.838838549"
+    street_addr     = "Hanover Quarter"
+    type            = "person"
+    zip             = "D02 RC90"
+  }
+
+  billing {
+    city            = "Dublin"
+    country         = "IE"
+    data_obfuscated = true
+    email           = "aredherring2+gandi@gmail.com"
+    extra_parameters = {
+      "birth_city"       = ""
+      "birth_country"    = ""
+      "birth_date"       = ""
+      "birth_department" = ""
+    }
+    family_name     = "Pantry"
+    given_name      = "Dan"
+    mail_obfuscated = true
+    phone           = "+353.838838549"
+    street_addr     = "Hanover Quarter"
+    type            = "person"
+    zip             = "D02 RC90"
+  }
+}
