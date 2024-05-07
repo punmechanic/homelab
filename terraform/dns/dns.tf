c8234edd6ed6f88de187b67914ddf6785c19a6ec
punmechanic <2939173+punmechanic@users.noreply.github.com>
2024-05-07 00:45:29 -0700

    Add Grafana

--- terraform/dns/dns.tf
+++ terraform/dns/dns.tf
@@ -15,3 +15,11 @@ resource "gandi_livedns_record" "gitea" {
   values = ["192.168.0.215"]
   ttl    = 36000
 }
+
+resource "gandi_livedns_record" "grafana" {
+  zone   = "aredherring.tech"
+  name   = "grafana"
+  type   = "A"
+  values = ["192.168.0.215"]
+  ttl    = 36000
+}
