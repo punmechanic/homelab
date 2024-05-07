c434a942c13d293eec6ff3ad6721165afa60325b
punmechanic <2939173+punmechanic@users.noreply.github.com>
2024-05-07 00:30:46 -0700

    Gitea does not support PKCE

--- terraform/keycloak/main.tf
+++ terraform/keycloak/main.tf
@@ -5,12 +5,13 @@ resource "keycloak_realm" "realm" {
 }
 
 resource "keycloak_openid_client" "gitea" {
-  realm_id                   = keycloak_realm.realm.id
-  client_id                  = "gitea"
-  name                       = "gitea.aredherring.tech"
-  access_type                = "CONFIDENTIAL"
-  valid_redirect_uris        = ["https://gitea.aredherring.tech/user/oauth2/keycloak/callback"]
-  pkce_code_challenge_method = "S256"
-  standard_flow_enabled      = true
-  consent_required           = true
+  realm_id            = keycloak_realm.realm.id
+  client_id           = "gitea"
+  name                = "gitea.aredherring.tech"
+  access_type         = "CONFIDENTIAL"
+  valid_redirect_uris = ["https://gitea.aredherring.tech/user/oauth2/keycloak/callback"]
+  # PKCE is not supported by Gitea OIDC.
+  # pkce_code_challenge_method = "S256"
+  standard_flow_enabled = true
+  consent_required      = true
 }
